e6ce375004a2ada5df9bf8213084d5056d4c03ed
titus.appel <titus.appel@localhost>
2011-04-12 20:59:24 -0600

    Works ok at home, don't know about Vicon

--- controllerISR.c
+++ controllerISR.c
@@ -14,15 +14,15 @@ extern int16_t linVelocity, angVelocity;
 
 int32_t e_lv_last = 0, e_av_last = 0, e_lv_sum = 0, e_av_sum = 0;
 int32_t u_lv = 0, u_av = 0;
-float kp_lv = 50, ki_lv = 0, kd_lv = 0;
+float kp_lv = 1000, ki_lv = 0, kd_lv = 0;
 float kp_av = 0.5, ki_av = 0.5, kd_av = 0.5;
 
 void ControllerPIDLoop(void)
 {
 	int32_t e_lv, e_av;
 
-//	if (StopLostConn == false)
-//	{
+	if (StopLostConn == false)
+	{
 		// Get the error signals
 		if (UseOdomComb)
 		{
@@ -39,8 +39,8 @@ void ControllerPIDLoop(void)
 		e_av_sum += e_av;
 
 		// Calculate the PID linear velocity control signal
-		u_lv = (uint32_t)(kp_lv * e_lv);// + ki_lv * e_lv_sum + kd_lv * (e_lv - e_lv_last));
-		u_av = (uint32_t)(kp_av * e_av);// + ki_av * e_av_sum + kd_av * (e_av - e_av_last));
+		u_lv = (uint32_t)(kp_lv * e_lv + ki_lv * e_lv_sum + kd_lv * (e_lv - e_lv_last));
+		u_av = (uint32_t)(kp_av * e_av + ki_av * e_av_sum + kd_av * (e_av - e_av_last));
 
 		// Set the PWM duty cycles for the motor and the steering servos
 		PWMSetDuty(MOTOR_CHANNEL, DUTY_1_5 + u_lv);
@@ -50,15 +50,15 @@ void ControllerPIDLoop(void)
 		// Store last velocity errors
 		e_lv_last = e_lv;
 		e_av_last = e_av;
-//	}
-//	else
-//	{
-//		PWMSetDuty(MOTOR_CHANNEL, DUTY_1_5);
-//		e_lv_last = 0;
-//		e_av_last = 0;
-//		e_lv_sum = 0;
-//		e_av_sum = 0;
-//		u_lv = 0;
-//		u_av = 0;
-//	}
+	}
+	else
+	{
+		PWMSetDuty(MOTOR_CHANNEL, DUTY_1_5);
+		e_lv_last = 0;
+		e_av_last = 0;
+		e_lv_sum = 0;
+		e_av_sum = 0;
+		u_lv = 0;
+		u_av = 0;
+	}
 }
