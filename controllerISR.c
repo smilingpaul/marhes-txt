d237f66ebc1e8297142ae186b1ababee32da8e3f
titus.appel <titus.appel@localhost>
2011-05-26 14:56:33 -0600

    Trying to figure out timings

--- controllerISR.c
+++ controllerISR.c
@@ -17,6 +17,7 @@ extern int32_t kp_av, ki_av, kd_av;
 
 int32_t e_lv_last = 0, e_av_last = 0, e_lv_sum = 0, e_av_sum = 0;
 int32_t u_lv = 0, u_av = 0;
+int32_t count = 0;
 
 void ControllerPIDLoop(void)
 {
@@ -44,7 +45,7 @@ void ControllerPIDLoop(void)
 		u_av = (int32_t)(kp_av * e_av + ki_av * e_av_sum + kd_av * (e_av - e_av_last));
 
 		// Set the PWM duty cycles for the motor and the steering servos
-		PWMSetDuty(MOTOR_CHANNEL, DUTY_1_5 + u_lv);
+		PWMSetDuty(MOTOR_CHANNEL, count++);//DUTY_1_5 + u_lv);
 	//	PWMSetDuty(FRONT_SERVO_CHANNEL, ControllerCalcPWM(FRONT_SERVO_CHANNEL));
 	//	PWMSetDuty(REAR_SERVO_CHANNEL, ControllerCalcPWM(REAR_SERVO_CHANNEL));
 
@@ -54,7 +55,10 @@ void ControllerPIDLoop(void)
 	}
 	else
 	{
-		PWMSetDuty(MOTOR_CHANNEL, DUTY_1_5);
+		count = count + 100;
+		if (count > 2147483647)
+			count = 0;
+		PWMSetDuty(MOTOR_CHANNEL, count);//DUTY_1_5);
 		e_lv_last = 0;
 		e_av_last = 0;
 		e_lv_sum = 0;
@@ -62,4 +66,5 @@ void ControllerPIDLoop(void)
 		u_lv = 0;
 		u_av = 0;
 	}
+	//FIO0PIN ^= (1<<21);
 }
